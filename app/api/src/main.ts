9c1bd629a020c1a123f38e05ba2d0a97cc9f24db
Hoang Phuc <hoangvanphuc16@gmail.com>
2026-04-25 10:51:09 +0700

    feat(security): add Helmet security middleware

--- app/api/src/main.ts
+++ app/api/src/main.ts
@@ -4,16 +4,18 @@ import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
 import { JwtAuthGuard } from './auth/passport/jwt-auth.guard';
 import { PoliciesGuard } from './auth/casl/policies.guard';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const port = configService.get('PORT');
   const reflector = app.get(Reflector);
+
   app.useGlobalGuards(new JwtAuthGuard(reflector));
   app.useGlobalGuards(new PoliciesGuard(reflector));
-
   app.useGlobalPipes(new ValidationPipe());
+  app.use(helmet());
 
   await app.listen(port);
 }
